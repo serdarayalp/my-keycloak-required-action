d70b1036aa18cd7549099997005bdee43288dcbe
Serdar Ayalp <serdar.ayalp@regiocom.com>
2024-01-02 14:24:51 +0100

    first commit

---
+++ src/main/java/de/mydomain/keycloak/MyRequiredActionFactory.java
@@ -0,0 +1,40 @@
+package de.mydomain.keycloak;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.RequiredActionFactory;
+import org.keycloak.authentication.RequiredActionProvider;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class MyRequiredActionFactory implements RequiredActionFactory {
+
+    @Override
+    public String getDisplayText() {
+        return null;
+    }
+
+    @Override
+    public RequiredActionProvider create(KeycloakSession keycloakSession) {
+        return null;
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+}
